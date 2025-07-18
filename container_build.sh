356f01edf95671e2ebde137590392c80f7395284
qm210 <quantenmace@gmail.com>
2025-07-19 00:17:39 +0200

    container just produces a .bin for now

---
+++ container_build.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# idea is to call this with some specific files to overwrite in /mnt
+# this will then produce a .bin to upload to your controller.
+
+# if stuff is there, copy it
+[ -f /mnt/my_config.h ] && cp -v /mnt/my_config.h wled00/
+[ -f /mnt/FX_DEADLINE_TROPHY.h ] && cp -v /mnt/FX_DEADLINE_TROPHY.h wled00/
+
+# fallback
+touch wled00/my_config.h
+
+# now let's platformio do its magic.
+platformio run
+
+# and publish the result
+cp -v .pio/build/deadline_trophy/firmware.bin /mnt/deadline_trophy_$(date +%Y%m%d_%H%M).bin
+
+# Maybe Zukunftsmusik: Manage the upload to the COM port from the container, like
+#   platformio run --target upload
+# but this needs permission to access the COM ports, blahblah, so yeah, not today.
